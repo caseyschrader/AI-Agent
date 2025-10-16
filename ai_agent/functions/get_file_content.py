ac88bcbfb64598ae2a29d2f75385ca2e045e9b3b
Casey Schrader <schradercasey15@gmail.com>
2025-10-16 21:11:47 +0000

    added get_file_contents function and tests

---
+++ ai_agent/functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+
+def get_file_content(working_directory, file_path):
+
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_dir):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+
+    try:
+
+        MAX_CHARS = 10000
+
+        with open(target_dir, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(target_dir) >= MAX_CHARS:
+                return f'{file_content_string} [...File "{file_path}" truncated at 10000 characters]'
+            return file_content_string 
+        
+    except Exception as e:
+        return f"Error: {e}"
+    
